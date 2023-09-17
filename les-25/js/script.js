26e3f818a053b8d911921a3c3e45dd1679d94731
Sylvian <sylvianjonckbloedt@gmail.com>
2023-09-17 22:18:38 +0200

    modified hover styles

--- les-25/js/script.js
+++ les-25/js/script.js
@@ -2,7 +2,15 @@ $(document).ready(function(){
       var show = $("#show");
       var close = $("#close");
       var cardInfo = $(".card-overlay");
-      
+
+      show.hover(function(){
+            $(".card-container>img").css("box-shadow","0 15px 15px 0px rgba(0, 0, 0, 0.25)");
+      },
+      function(){
+            $(".card").css("box-shadow", "none");
+            $(".card-container>img").css("box-shadow","none");
+      });
+
       show.click(function(){
             cardInfo.slideDown(200);
       });
